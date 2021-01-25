6d720c2998dab9c756e146336a8350d26c6d8577
throzyrt <tahirozyurt78@gmail.com>
2021-01-25 18:20:22 +0300

    Add project files.

---
+++ Metodlar/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodlar
+{
+    class SepetManager
+    {
+        //naming convention:isimlendirme kuralı
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler.Sepete Eklendi : " + urun.Adi);
+
+        }
+        public void Ekle2(string urunAdi,string aciklama,double fiyat,int stokAdet)
+        {
+            Console.WriteLine("Tebrikler.Sepete Eklendi : " + urunAdi);
+
+        }
+    }
+}
